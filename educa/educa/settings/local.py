49fc1128d02b1ee4c4190975971d0864837d987d
anlapaev <62844625+anlapaev@users.noreply.github.com>
2025-06-08 15:07:13 +0300

    Configure SMTP email in local settings

--- educa/educa/settings/local.py
+++ educa/educa/settings/local.py
@@ -1,3 +1,4 @@
+from decouple import config
 from .base import *
 
 # Allow local origins so WebSocket connections succeed when using
@@ -29,3 +30,12 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+# Email configuration for password reset and other emails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
