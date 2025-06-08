b6a20e4993bcf065c3e32453f01a4126d0af0f39
anlapaev <62844625+anlapaev@users.noreply.github.com>
2025-06-08 13:55:54 +0300

    Add dynamic module form addition

--- educa/courses/forms.py
+++ educa/courses/forms.py
@@ -1,10 +1,20 @@
-from django.forms.models import inlineformset_factory
+from django.forms.models import inlineformset_factory, BaseInlineFormSet
 
 from .models import Course, Module
+
+
+class ModuleBaseFormSet(BaseInlineFormSet):
+    """Formset for modules with Russian delete label."""
+
+    def add_fields(self, form, index):
+        super().add_fields(form, index)
+        if self.can_delete and 'DELETE' in form.fields:
+            form.fields['DELETE'].label = 'Удалить'
 
 ModuleFormSet = inlineformset_factory(
     Course,
     Module,
+    formset=ModuleBaseFormSet,
     fields=['title', 'description'],
     extra=2,
     can_delete=True,
